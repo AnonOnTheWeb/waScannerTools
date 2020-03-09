10703f0901fa8f421feec643fedb6b47fa7c95ec
Joshek <joshua.bishop1974@gmail.com>
2020-03-09 15:03:44 +0800

    some tweaks and changes
    - you can now define how often you'd like the feed to refresh with the refreshTime value on line 6 (default is 30 seconds)
    - removed the incident summary since that prints out with RSS feed keys, will replace eventually.
    - terminal now clears after a feed refresh, keeps the traceback cleaner.
    - pruned some of the redundant code and comments

--- EmergencyWA_Feed.py
+++ EmergencyWA_Feed.py
@@ -1,8 +1,9 @@
 import feedparser # tested and ran on Python 2.7.17 and 3.6.9
-import datetime, time # script will automatically refresh every 30 seconds, newest incidents are pushed to the bottom of the list, and older incidents are further up
+import datetime, time, os
 from time import strftime
 from datetime import date
 
+refreshTime=30 # set how often you'd like the feed to refresh (in seconds)
 incidentFeed = feedparser.parse("https://www.emergency.wa.gov.au/data/incident_FCAD.rss")
 inc = incidentFeed.entries[1]
 
@@ -18,7 +19,6 @@ class colorText:
 
 currentTime = strftime("%Y-%m-%d %H:%M:%S")
 incTitle = colorText.BOLD + colorText.UNDERLINE + colorText.EMERGENCY
-preamble = colorText.HEADER + colorText.BOLD
 resetText = colorText.ENDC
 
 def newLine(): # shortcut to print a new line
@@ -32,9 +32,7 @@ def getIncidents(): # print incident information
         print(colorText.GREEN + "Incident page: " + inc.link) # link to EmergencyWA page for incident
         print("Incident reported: " + inc.published) # incident creation timestamp
         newLine()
-        print(colorText.WARNING + str(inc.summary) + colorText.GREEN) # incident summary
-        newLine()
-        print("Coordinates: " + inc.geo_lat + " " + inc.geo_long) # incident coordinates
+        print(colorText.WARNING + "Coordinates: " + inc.geo_lat + " " + inc.geo_long) # incident coordinates
         print("Map: https://www.google.com/maps/@" + inc.geo_lat + "," + inc.geo_long + ",15z") # incident location via google maps
         print(resetText + "===========================================================================")
         newLine()
@@ -42,11 +40,12 @@ def getIncidents(): # print incident information
 # start of program runtime
 while True:
     getIncidents() # calls the getIncidents function and prints all active incidents
-    print(preamble + "EmergencyWA active incidents") # print stats message
+    print(colorText.HEADER + colorText.BOLD + "EmergencyWA active incidents") # print stats message
     newLine()
     print("Current time: " + colorText.BLUE + strftime("%Y-%m-%d %H:%M:%S") + preamble)
     print("Current incidents: " + colorText.BLUE + str(len(incidentFeed.entries)) + resetText)
     newLine()
-    time.sleep(30)
+    time.sleep(refreshTime)
+    os.system("clear")
 
 resetText
